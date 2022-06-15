691b0715eafbbc73574d2f020223c686dab48f27
junior <abelsouzacosta@gmail.com>
2022-06-14 22:56:56 -0300

    [Chore]: adding auth-api

---
+++ auth-api/app.mjs
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const { env } = process;
+const PORT = env.PORT || 8080;
+
+app.get('/', (req, res) => res.redirect);
+
+app.get('/api/status', (req, res) =>
+  res.status(200).json({
+    service: 'Auth Service',
+    status: 'up',
+    httpStatus: 200,
+  }),
+);
+
+app.listen(PORT, () => console.info(`Server listening on port ${PORT}`));
+
+export default app;
