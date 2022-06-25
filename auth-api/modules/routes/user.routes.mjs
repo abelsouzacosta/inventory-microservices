1645572e64b7f8df92ce1f99b5a42bd49c69f9bc
junior <abelsouzacosta@gmail.com>
2022-06-25 05:27:15 -0300

    [Refactor]: rename directory to pipes

--- auth-api/modules/routes/user.routes.mjs
+++ auth-api/modules/routes/user.routes.mjs
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import controller from '../controllers/users/UserController.mjs';
-import emailPipe from '../pipe/EmailAlreadyExistsPipe.mjs';
+import emailPipe from '../pipes/EmailAlreadyExistsPipe.mjs';
 
 const router = Router();
 
