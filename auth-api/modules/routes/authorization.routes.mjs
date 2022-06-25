eec30ef4189cf0556bf23275459002ab92a9fb45
junior <abelsouzacosta@gmail.com>
2022-06-25 06:00:08 -0300

    [Feat][Refactor](users): pipes
    adding pipe for user validation

--- auth-api/modules/routes/authorization.routes.mjs
+++ auth-api/modules/routes/authorization.routes.mjs
@@ -1,8 +1,9 @@
 import { Router } from 'express';
 import controller from '../controllers/authorization/AuthController.mjs';
+import userNotFoundPipe from '../pipes/UserNotFoundPipe.mjs';
 
 const authRouter = Router();
 
-authRouter.post('/sign-in', controller.authorize);
+authRouter.post('/sign-in', userNotFoundPipe.execute, controller.authorize);
 
 export default authRouter;
--- auth-api/modules/routes/user.routes.mjs
+++ auth-api/modules/routes/user.routes.mjs
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import controller from '../controllers/users/UserController.mjs';
-import emailPipe from '../pipes/EmailAlreadyExistsPipe.mjs';
+import emailAlreadyExistsPipe from '../pipes/EmailAlreadyExistsPipe.mjs';
 
 const router = Router();
 
 router.get('/', controller.list);
 
-router.post('/', emailPipe.execute, controller.create);
+router.post('/', emailAlreadyExistsPipe.execute, controller.create);
 
 export default router;
