70122fac5fa4d560425198e0a85ad5af07f0acc8
junior <abelsouzacosta@gmail.com>
2022-07-05 08:38:12 -0300

    [Chore](category): adding list method to the repository

--- products-api/modules/repositories/CategoryRepository.mjs
+++ products-api/modules/repositories/CategoryRepository.mjs
@@ -1,6 +1,12 @@
 import Category from '../models/Category.mjs';
 
 class CategoryRepository {
+  async list() {
+    const categories = await Category.findAll();
+
+    return categories;
+  }
+
   async create(name) {
     const category = await Category.create({
       name,
