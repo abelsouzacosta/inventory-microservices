082e441d07253b826c6d2816130da3fa4aad6330
junior <abelsouzacosta@gmail.com>
2022-07-05 08:58:51 -0300

    adding method list to the controller

--- products-api/modules/controllers/category/CategoryController.mjs
+++ products-api/modules/controllers/category/CategoryController.mjs
@@ -1,6 +1,12 @@
 import service from '../../services/category/CategoryService.mjs';
 
 class CategoryController {
+  async list(req, res) {
+    const categories = await service.list();
+
+    return res.status(200).json(categories);
+  }
+
   async create(req, res) {
     const { name } = req.body;
 
