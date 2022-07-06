3e35f68f4b677fac3f41070a04f47c3ad4fccef8
Abel Souza Costa Junior <abesouzacosta@mail.com>
2022-07-06 18:49:03 -0300

    [Feat](pipes): CategoryNotFoundPipe
    searchs for a pipe ion the database through an id given in the request param

--- products-api/modules/routes/category.routes.mjs
+++ products-api/modules/routes/category.routes.mjs
@@ -1,16 +1,17 @@
 import { Router } from 'express';
 import controller from '../controllers/category/CategoryController.mjs';
+import categoryNotFoundPipe from '../pipes/category/CategoryNotFoundPipe.mjs';
 
 const router = Router();
 
-router.get('/:id', controller.getCategory);
+router.get('/:id', categoryNotFoundPipe.execute, controller.getCategory);
 
 router.get('/', controller.list);
 
 router.post('/', controller.create);
 
-router.put('/:id', controller.update);
+router.put('/:id', categoryNotFoundPipe.execute, controller.update);
 
-router.delete('/:id', controller.delete);
+router.delete('/:id', categoryNotFoundPipe.execute, controller.delete);
 
 export default router;
