084f6b084f073c872763e79b9bf8a5853ab0dc57
Abel Souza Costa Junior <abesouzacosta@mail.com>
2022-07-06 18:38:56 -0300

    [Feat](errors): adding error handler

--- products-api/app.mjs
+++ products-api/app.mjs
@@ -1,11 +1,13 @@
 import 'express-async-errors';
 import express from 'express';
+import errorHandler from './shared/infra/errors/middlewares/ErrorHandler.mjs';
 import router from './modules/routes/index.routes.mjs';
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(errorHandler);
 
 const { env } = process;
 const PORT = env.PORT || 3030;
