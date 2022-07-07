c111533da14bf7a8fd2b8d96255868ac65d24c71
abelsouzacosta <abelsouzacosta@gmail.com>
2022-07-07 18:43:27 -0300

    [Chore](product): adding method create to service

--- products-api/modules/services/product/ProductService.mjs
+++ products-api/modules/services/product/ProductService.mjs
@@ -4,6 +4,10 @@ class ProductService {
   async list() {
     return repository.list();
   }
+
+  async create(name, ean, supplier_id) {
+    return repository.create(name, ean, supplier_id);
+  }
 }
 
 export default new ProductService();
