a62ccdf600514efdf06c18ec8da9099dc7d07f68
abelsouzacosta <abelsouzacosta@gmail.com>
2022-07-08 20:37:50 -0300

    [Chore](supplier): adding pipe to deal with no existent supplier

---
+++ products-api/modules/pipes/supplier/SupplierNotFoundPipe.mjs
@@ -0,0 +1,21 @@
+import repository from '../../repositories/SupplierRepository.mjs';
+import HttpStatus from '../../../shared/infra/constants/server/HttpStatus.mjs';
+import ApplicationError from '../../../shared/errors/ApplicationError.mjs';
+
+class SupplierNotFoundPipe {
+  async execute(req, res, next) {
+    const { id } = req.params;
+
+    const supplierExists = await repository.findById(id);
+
+    if (!supplierExists)
+      throw new ApplicationError(
+        `Supplier not found with id ${id}`,
+        HttpStatus.NOT_FOUND,
+      );
+
+    return next();
+  }
+}
+
+export default new SupplierNotFoundPipe();
