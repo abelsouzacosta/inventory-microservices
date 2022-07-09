2ff5cc443f9c8b491d4439dc4da143bb1b4c9bd7
abelsouzacosta <abelsouzacosta@gmail.com>
2022-07-09 00:41:21 -0300

    [Feat](products): pipe
    adding pipe to validate if the ean is already taken by another instance

--- products-api/modules/routes/product.routes.mjs
+++ products-api/modules/routes/product.routes.mjs
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import controller from '../controllers/product/ProductController.mjs';
 import supplierNotFoundPipe from '../pipes/supplier/SupplierNotFoundPipe.mjs';
 import eanAlredyExistsPipe from '../pipes/product/EanAlredyExistsPipe.mjs';
+import eanAlreadyTakenPipe from '../pipes/product/EanAlreadyTakenPipe.mjs';
 
 const router = Router();
 
@@ -16,7 +17,7 @@ router.post(
   controller.create,
 );
 
-router.put('/:id', controller.update);
+router.put('/:id', eanAlreadyTakenPipe.execute, controller.update);
 
 router.patch('/:id', supplierNotFoundPipe.execute, controller.updateSupplier);
 
