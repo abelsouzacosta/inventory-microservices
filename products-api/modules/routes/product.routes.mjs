2905d7181263583492d18ce1369356657fd84a27
abelsouzacosta <abelsouzacosta@gmail.com>
2022-07-10 09:11:08 -0300

    [Feat](prodcuts): pipe
    pipe to deal withnon existent products

--- products-api/modules/routes/product.routes.mjs
+++ products-api/modules/routes/product.routes.mjs
@@ -3,6 +3,7 @@ import controller from '../controllers/product/ProductController.mjs';
 import supplierNotFoundPipe from '../pipes/supplier/SupplierNotFoundPipe.mjs';
 import eanAlredyExistsPipe from '../pipes/product/EanAlredyExistsPipe.mjs';
 import eanAlreadyTakenPipe from '../pipes/product/EanAlreadyTakenPipe.mjs';
+import productNotFoundPipe from '../pipes/product/ProductNotFoundPipe.mjs';
 
 const router = Router();
 
@@ -17,10 +18,20 @@ router.post(
   controller.create,
 );
 
-router.put('/:id', eanAlreadyTakenPipe.execute, controller.update);
+router.put(
+  '/:id',
+  productNotFoundPipe.execute,
+  eanAlreadyTakenPipe.execute,
+  controller.update,
+);
 
-router.patch('/:id', supplierNotFoundPipe.execute, controller.updateSupplier);
+router.patch(
+  '/:id',
+  productNotFoundPipe.execute,
+  supplierNotFoundPipe.execute,
+  controller.updateSupplier,
+);
 
-router.delete('/:id', controller.delete);
+router.delete('/:id', productNotFoundPipe.execute, controller.delete);
 
 export default router;
