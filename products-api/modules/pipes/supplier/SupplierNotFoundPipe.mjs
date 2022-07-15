7553f2d0423c5b3baaccc83d9d381d8a6f622850
Abel Junior <abel.costa@webjump.com.br>
2022-07-14 23:30:21 -0300

    [Chore]: logging

--- products-api/modules/pipes/supplier/SupplierNotFoundPipe.mjs
+++ products-api/modules/pipes/supplier/SupplierNotFoundPipe.mjs
@@ -1,6 +1,7 @@
 import repository from '../../repositories/SupplierRepository.mjs';
 import HttpStatus from '../../../shared/infra/constants/server/HttpStatus.mjs';
 import ApplicationError from '../../../shared/errors/ApplicationError.mjs';
+import logger from '../../../shared/infra/logger/logger.mjs';
 
 class SupplierNotFoundPipe {
   async execute(req, res, next) {
@@ -8,12 +9,14 @@ class SupplierNotFoundPipe {
 
     const supplierExists = await repository.findById(id);
 
-    if (!supplierExists)
+    if (!supplierExists) {
+      logger.error(`supplier ${id} not found`);
+
       throw new ApplicationError(
         `Supplier not found with id ${id}`,
         HttpStatus.NOT_FOUND,
       );
-
+    }
     return next();
   }
 }
