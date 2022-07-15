6e1f1c0d3a79bf4fe190e023175ccc7cab580b17
Abel Junior <abel.costa@webjump.com.br>
2022-07-14 23:30:04 -0300

    [Chore]: logging

--- products-api/modules/controllers/supplier/SupplierController.mjs
+++ products-api/modules/controllers/supplier/SupplierController.mjs
@@ -1,9 +1,12 @@
 import HttpStatus from '../../../shared/infra/constants/server/HttpStatus.mjs';
 import service from '../../services/supplier/SupplierService.mjs';
+import logger from '../../../shared/infra/logger/logger.mjs';
 
 class SupplierController {
   async list(req, res) {
     const suppliers = await service.list();
+
+    logger.info(`getting all suppliers`);
 
     return res.status(HttpStatus.OK).json(suppliers);
   }
@@ -13,6 +16,8 @@ class SupplierController {
 
     const supplier = await service.getOneBy(id);
 
+    logger.info(`getting supplier ${id}`);
+
     return res.status(HttpStatus.OK).json(supplier);
   }
 
@@ -21,12 +26,16 @@ class SupplierController {
 
     const supplier = await service.create(name, phone);
 
+    logger.info(`creating a new supplier`);
+
     return res.status(HttpStatus.CREATED).json(supplier);
   }
 
   async update(req, res) {
     const { id } = req.params;
     const { name, phone } = req.body;
+
+    logger.info(`updating supplier ${id}`);
 
     const supplier = await service.update(id, name, phone);
 
@@ -35,6 +44,8 @@ class SupplierController {
 
   async delete(req, res) {
     const { id } = req.params;
+
+    logger.info(`deleting supplier ${id}`);
 
     await service.delete(id);
 
