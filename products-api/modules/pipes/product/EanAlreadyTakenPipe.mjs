5da388a11b31a0f1aa2fb19ccd6ba4a0566bc56d
Abel Junior <abel.costa@webjump.com.br>
2022-07-14 23:30:09 -0300

    [Chore]: logging

--- products-api/modules/pipes/product/EanAlreadyTakenPipe.mjs
+++ products-api/modules/pipes/product/EanAlreadyTakenPipe.mjs
@@ -1,6 +1,7 @@
 import repository from '../../repositories/ProductRepository.mjs';
 import ApplicationError from '../../../shared/errors/ApplicationError.mjs';
 import HttpStatus from '../../../shared/infra/constants/server/HttpStatus.mjs';
+import logger from '../../../shared/infra/logger/logger.mjs';
 
 class EanAlreadyTaken {
   async execute(req, res, next) {
@@ -10,11 +11,14 @@ class EanAlreadyTaken {
     const product = await repository.findById(id);
     const eanExists = await repository.findByEan(ean);
 
-    if (product.id !== eanExists.id)
+    if (product.id !== eanExists.id) {
+      logger.error(`ean ${ean} is already taken by another instance`);
+
       throw new ApplicationError(
         `Ean ${ean} already belongs to another instance of product with id ${eanExists.id}`,
         HttpStatus.CONFLICT,
       );
+    }
 
     return next();
   }
