673a55e6eb214051b1d6bf246cc8aeb0f8644520
Abel Junior <abel.costa@webjump.com.br>
2022-07-14 23:30:13 -0300

    [Chore]: logging

--- products-api/modules/pipes/product/EanAlredyExistsPipe.mjs
+++ products-api/modules/pipes/product/EanAlredyExistsPipe.mjs
@@ -1,6 +1,7 @@
 import repository from '../../repositories/ProductRepository.mjs';
 import ApplicationError from '../../../shared/errors/ApplicationError.mjs';
 import HttpStatus from '../../../shared/infra/constants/server/HttpStatus.mjs';
+import logger from '../../../shared/infra/logger/logger.mjs';
 
 class EanAlreadyExistsPipe {
   async execute(req, res, next) {
@@ -8,12 +9,14 @@ class EanAlreadyExistsPipe {
 
     const eanAlreadyExists = await repository.findByEan(ean);
 
-    if (eanAlreadyExists)
+    if (eanAlreadyExists) {
+      logger.error(`ean ${ean} already exists in the database`);
+
       throw new ApplicationError(
         `Ean ${ean} alredy exists in the database`,
         HttpStatus.CONFLICT,
       );
-
+    }
     return next();
   }
 }
