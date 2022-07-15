2743fed8eec561f929a6a74a614f343abb97adcc
Abel Junior <abel.costa@webjump.com.br>
2022-07-14 23:30:16 -0300

    [Chore]: logging

--- products-api/modules/pipes/product/ProductNotFoundPipe.mjs
+++ products-api/modules/pipes/product/ProductNotFoundPipe.mjs
@@ -1,6 +1,7 @@
 import repository from '../../repositories/ProductRepository.mjs';
 import ApplicationError from '../../../shared/errors/ApplicationError.mjs';
 import HttpStatus from '../../../shared/infra/constants/server/HttpStatus.mjs';
+import logger from '../../../shared/infra/logger/logger.mjs';
 
 class ProductNotFoundPipe {
   async execute(req, res, next) {
@@ -8,11 +9,14 @@ class ProductNotFoundPipe {
 
     const productFound = await repository.findById(id);
 
-    if (!productFound)
+    if (!productFound) {
+      logger.error(`product ${id} not found`);
+
       throw new ApplicationError(
         `Product not found with id ${id}`,
         HttpStatus.NOT_FOUND,
       );
+    }
 
     return next();
   }
