ff583c81002dd19f9baed9c9d1fec66d762877da
Abel Junior <abel.costa@webjump.com.br>
2022-07-14 22:38:15 -0300

    [Refactor](logger): addinc classe to use loggins

--- products-api/shared/infra/logger/logger.mjs
+++ products-api/shared/infra/logger/logger.mjs
@@ -1,21 +1,33 @@
 import { createLogger, format, transports } from 'winston';
 
-const logger = createLogger({
-  level: 'info',
-  format: format.combine(
-    format.timestamp({
-      format: 'DD-MM-YYYY HH:mm:ss',
-    }),
-    format.errors({ stack: true }),
-    format.splat(),
-    format.json(),
-  ),
-  defaultMeta: { service: 'products-api' },
-  transports: [
-    new transports.Console({
-      format: format.combine(format.colorize(), format.simple()),
-    }),
-  ],
-});
+class Logger {
+  constructor() {
+    this.logger = createLogger({
+      level: 'info',
+      format: format.combine(
+        format.timestamp({
+          format: 'DD-MM-YYYY HH:mm:ss',
+        }),
+        format.errors({ stack: true }),
+        format.splat(),
+        format.json(),
+      ),
+      defaultMeta: { service: 'products-api' },
+      transports: [
+        new transports.Console({
+          format: format.combine(format.colorize(), format.simple()),
+        }),
+      ],
+    });
+  }
 
-export default logger;
+  info(msg) {
+    return this.logger.info(msg);
+  }
+
+  error(msg) {
+    return this.logger.error(msg);
+  }
+}
+
+export default new Logger();
