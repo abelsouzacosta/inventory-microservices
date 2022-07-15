de68aacb946457a47ba5026a11bd995def97e942
Abel Junior <abel.costa@webjump.com.br>
2022-07-14 23:29:58 -0300

    [Chore]: logging

--- products-api/modules/controllers/product/ProductController.mjs
+++ products-api/modules/controllers/product/ProductController.mjs
@@ -1,15 +1,20 @@
 import service from '../../services/product/ProductService.mjs';
 import HttpStatus from '../../../shared/infra/constants/server/HttpStatus.mjs';
+import logger from '../../../shared/infra/logger/logger.mjs';
 
 class ProductController {
   async list(req, res) {
     const products = await service.list();
+
+    logger.info('getting all products');
 
     return res.status(HttpStatus.OK).json(products);
   }
 
   async getProduct(req, res) {
     const { id } = req.params;
+
+    logger.info(`getting product ${id}`);
 
     const product = await service.getOneProduct(id);
 
@@ -18,6 +23,8 @@ class ProductController {
 
   async create(req, res) {
     const { name, ean, supplier_id } = req.body;
+
+    logger.info(`creating product`);
 
     const product = await service.create(name, ean, supplier_id);
 
@@ -28,6 +35,8 @@ class ProductController {
     const { id } = req.params;
     const { name, ean } = req.body;
 
+    logger.info(`updating category ${id}`);
+
     const product = await service.update(id, name, ean);
 
     return res.status(HttpStatus.CREATED).json(product);
@@ -37,6 +46,8 @@ class ProductController {
     const { id } = req.params;
     const { supplier_id } = req.body;
 
+    logger.info(`updating supplier of the product ${id} to ${supplier_id}`);
+
     const product = await service.updateSupplier(id, supplier_id);
 
     return res.status(HttpStatus.CREATED).json(product);
@@ -44,6 +55,8 @@ class ProductController {
 
   async delete(req, res) {
     const { id } = req.params;
+
+    logger.info(`deleting product ${id}`);
 
     await service.delete(id);
 
