976b30d20fda1ed5a6ae51ac67f77d62dfffef32
Abel Junior <abel.costa@webjump.com.br>
2022-07-18 16:39:53 -0300

    [Feat](category): pipes
    Fixes #10

--- products-api/modules/routes/category.routes.mjs
+++ products-api/modules/routes/category.routes.mjs
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import controller from '../controllers/category/CategoryController.mjs';
 import categoryNotFoundPipe from '../pipes/category/CategoryNotFoundPipe.mjs';
+import categoryAlreadyExistsPipe from '../pipes/category/CategoryAlreadyExistsPipe.mjs';
+import categoryNameAlreadyTakenPipe from '../pipes/category/CategoryNameAlreadyTakenPipe.mjs';
 
 const router = Router();
 
@@ -8,9 +10,14 @@ router.get('/:id', categoryNotFoundPipe.execute, controller.getCategory);
 
 router.get('/', controller.list);
 
-router.post('/', controller.create);
+router.post('/', categoryAlreadyExistsPipe.execute, controller.create);
 
-router.put('/:id', categoryNotFoundPipe.execute, controller.update);
+router.put(
+  '/:id',
+  categoryNotFoundPipe.execute,
+  categoryNameAlreadyTakenPipe.execute,
+  controller.update,
+);
 
 router.delete('/:id', categoryNotFoundPipe.execute, controller.delete);
 
